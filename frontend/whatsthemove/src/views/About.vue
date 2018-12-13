d9ab2374d6c01505005640ba75207013727e9e0e
Phillip Stratos <phillipstratos@gmail.com>
2018-12-13 13:08:25 -0500

    Draft Frontend

--- frontend/whatsthemove/src/views/About.vue
+++ frontend/whatsthemove/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>Hello, Welcome to What's the Move! We are just a few college students who are dedicated to letting the world know that Finnegans Cove has happy hour from 2-9, or should I say happy hours, LOL! I guess we are also interested in other local hotspots around town, so please take a look to find out "What's the Move?!"</h1>
-  </div>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/wtm.png')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          What is What's The Move?
+        </h1>
+
+        <div class="about">
+          <h2>Hello, welcome to What's the Move?! We are just a few college students who are dedicated to letting the world know that Finnegan's Cove has happy hour from 2-9 pm, or should I say happy hours... LOL! I guess we are also interested in other local hotspots around town too, so please take a look to find out "What's the Move?!"</h2>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
--- frontend/whatsthemove/src/views/calendar/calendar.vue
+++ frontend/whatsthemove/src/views/calendar/calendar.vue
@@ -19,7 +19,7 @@
         </v-btn>
       </v-card-title>
       <v-img
-        src="https://cdn.vuetifyjs.com/images/cards/forest.jpg"
+        src="https://www.hopsmeetbarley.com/wp-content/uploads/2017/12/Happy-Hour2-min.png"
         gradient="to top, rgba(0,0,0,.44), rgba(0,0,0,.44)"
         height="200"
       >
@@ -40,41 +40,83 @@
         dense
       >
         <v-timeline-item
-          color="pink"
+          color="red"
           large
         >
           <v-layout pt-3>
+            <v-flex xs3>
+              <strong>2-9pm</strong>
+            </v-flex>
+            <v-flex>
+              <strong>Finnegan's Cove</strong>
+              <div class="caption">$4 Rail Drinks</div>
+            </v-flex>
+            <v-spacer></v-spacer>
+            <v-flex>
+              <v-card-actions class="pa-3">
+              <h2> Rating </h2>
+              <span class="grey--text text--lighten-2 caption mr-2">
+                ({{ rating }})
+              </span>
+                <v-rating
+                  v-model="rating"
+                  background-color="pa-2 red"
+                  color="red"
+                  dense
+                  half-increments
+                  hover
+                  size="18"
+              ></v-rating>
+              </v-card-actions>
+            </v-flex>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+          </v-layout>
+        <v-divider class="dark"></v-divider>
+        </v-timeline-item>
+        
+        <v-timeline-item
+          color="primary"
+          large
+        >
+          <v-layout wrap pt-3>
             <v-flex xs3>
               <strong>5-8pm</strong>
             </v-flex>
             <v-flex>
               <strong>Brickhouse Tavern</strong>
-              <div class="caption">1/2 off Pitchers</div>
+              <div class="caption mb-2">1/2 off Pitchers</div>
             </v-flex>
-          </v-layout>
-        </v-timeline-item>
-
-        <v-timeline-item
-          color="teal lighten-3"
-          large
-        >
-          <v-layout wrap pt-3>
-            <v-flex xs3>
-              <strong>2-9pm</strong>
-            </v-flex>
+            <v-spacer></v-spacer>
             <v-flex>
-<<<<<<< HEAD
-              <strong>Brickhouse Tavern</strong>
-=======
-              <strong>Finnegans Cove</strong>
->>>>>>> de624ec090cb5f54bac376069a1b0f847240bc45
-              <div class="caption mb-2">$4 Rail Drinks</div>
+              <v-card-actions class="pa-3">
+              <h2> Rating </h2>
+              <span class="grey--text text--lighten-2 caption mr-2">
+                ({{ rating }})
+              </span>
+                <v-rating
+                  v-model="rating"
+                  background-color="pa-2 red"
+                  color="red"
+                  dense
+                  half-increments
+                  hover
+                  size="18"
+              ></v-rating>
+              </v-card-actions>
             </v-flex>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
           </v-layout>
+        <v-divider class="dark"></v-divider>
         </v-timeline-item>
 
         <v-timeline-item
-          color="pink"
+          color="red"
           large
         >
           <v-layout pt-3>
@@ -85,11 +127,34 @@
               <strong>Buffalo Wild Wings</strong>
               <div class="caption">$5 for 20 wings</div>
             </v-flex>
+            <v-spacer></v-spacer>
+            <v-flex>
+              <v-card-actions class="pa-3">
+              <h2> Rating </h2>
+              <span class="grey--text text--lighten-2 caption mr-2">
+                ({{ rating }})
+              </span>
+                <v-rating
+                  v-model="rating"
+                  background-color="pa-2 red"
+                  color="red"
+                  dense
+                  half-increments
+                  hover
+                  size="18"
+              ></v-rating>
+              </v-card-actions>
+            </v-flex>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
           </v-layout>
+        <v-divider class="dark"></v-divider>
         </v-timeline-item>
 
         <v-timeline-item
-          color="teal lighten-3"
+          color="primary"
           large
         >
           <v-layout pt-3>
@@ -100,7 +165,30 @@
               <strong>Gold Crown</strong>
               <div class="caption">First beer is 1¢, the rest are 50¢</div>
             </v-flex>
+            <v-spacer></v-spacer> 
+            <v-flex>
+              <v-card-actions class="pa-3">
+              <h2> Rating </h2>
+              <span class="grey--text text--lighten-2 caption mr-2">
+                ({{ rating }})
+              </span>
+                <v-rating
+                  v-model="rating"
+                  background-color="pa-2 red"
+                  color="red"
+                  dense
+                  half-increments
+                  hover
+                  size="18"
+              ></v-rating>
+              </v-card-actions>
+            </v-flex>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
           </v-layout>
+        <v-divider class="dark"></v-divider>
         </v-timeline-item>
       </v-timeline>
     </v-card-text>
