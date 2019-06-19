1374bf3dce499433e6d943820407e20e0a9b3c61
Chris Jones <chris@ctjones.com>
2019-06-19 10:49:49 -0400

    new updates to backend

--- backend/src/middleware/index.js
+++ backend/src/middleware/index.js
@@ -6,5 +6,5 @@ module.exports = function (app) {
 const calendar = require('./calendar/calendar.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
-  app.configure(courses);
+  app.configure(calendar);
 };
